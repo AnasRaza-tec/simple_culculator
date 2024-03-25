983f8c548e66578b9d443392f2082d633c77ecea
AnasRaza-tec <157498271+AnasRaza-tec@users.noreply.github.com>
2024-03-26 02:09:48 +0500

    fisrt program

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstnumber" },
+    { message: "enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one to the oprators to perform opration",
+        type: "list",
+        name: "oprator",
+        choices: ["addition", "subraction", "multipilcation", "division"],
+    },
+]);
+if (answer.oprator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.oprator === "subraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.oprator === "multipilcation") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.oprator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please select valid oprator");
+}
